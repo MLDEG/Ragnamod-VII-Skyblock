6f1fe47e31a63b1f97c1a49b52011fc9f799c95e
MLDEG <mrmldeg@gmail.com>
2026-04-30 20:23:23 +0200

    added Pink Slime alt recipe

---
+++ kubejs/server_scripts/industrialforegoing/laser_drill_fluid.js
@@ -0,0 +1,24 @@
+onEvent('recipes', e => {
+
+    //Pink Slime
+    e.custom({
+        "output": "{FluidName:\"industrialforegoing:pink_slime\",Amount:10}",
+        "rarity": [
+            {
+                "whitelist": {},
+                "blacklist": {},
+                "depth_min": -64,
+                "depth_max": 256,
+                "weight": 8
+            }
+        ],
+        "pointer": 0,
+        "catalyst": {
+            "item": "industrialforegoing:laser_lens6"
+        },
+        "entity": "botania:pink_wither",
+        "type": "industrialforegoing:laser_drill_fluid"
+    })
+
+
+})
